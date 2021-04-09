9fe1aea01e6afd82f5ac4264736912a83eb0e35d
Keil Reedich <keilreedich@Keils-MacBook-Air.local>
2021-04-09 01:01:16 -0400

    final

--- src/box.h
+++ src/box.h
@@ -6,7 +6,7 @@
 
 class box : public hittable {
 public:
-   box() : c(0), ax(0), ay(0), az(0), hx(0), hy(0), hz(0), mat_ptr(0) {}
+   box() : c(0), ax(0), ay(0), az(0), hx(0), hy(0), hz(0), mat_ptr(0) {};
    box(const glm::point3& center, 
        const glm::vec3& xdir, const glm::vec3& ydir, const glm::vec3& zdir,
        const glm::vec3& halfx, const glm::vec3& halfy, const glm::vec3& halfz,
@@ -15,8 +15,49 @@ public:
 
    virtual bool hit(const ray& r, hit_record& rec) const override
    {
-      // todo
-      return false;
+      float t;
+      float tmin = -infinity;
+      float tmax = infinity;
+      vec3 p = c - r.origin(); 
+      vec3 a[3] = {ax,ay,az};
+      vec3 h[3] = {hx,hy,hz};
+
+      for(int i = 0; i<3; i++){
+         float e = glm::dot(a[i], p);
+         float f = glm::dot(a[i], r.direction());
+         if (abs(f) > std::numeric_limits<double>::epsilon()){
+            float t1 = (e+length(h[i]))/f;
+            float t2 = (e-length(h[i]))/f; 
+            if (t1 > t2) std::swap(t1, t2);
+            if (t1 > tmin) {
+               tmin = t1;
+               rec.set_face_normal(r, a[i]); 
+            }
+            if (t2 < tmax) {
+               tmax = t2;
+               rec.set_face_normal(r, a[i]);
+            }
+            if (tmin > tmax) return false;
+            if (tmax < 0) return false; 
+         }
+         else if ((0-e-length(h[i])) > 0 || (0-e+length(h[i])) < 0) {
+            return false;
+         }
+      }
+      if(tmin>0) t = tmin;
+      else t = tmax;
+
+      // save relevant data in hit record
+      rec.t = t; // save the time when we hit the object
+      rec.p = r.at(t); // ray.origin + t * ray.direction
+      rec.mat_ptr = mat_ptr; 
+
+      /*// save normal
+      float len = length(rec.p-c);
+      glm::vec3 outward_normal = (rec.p - c)/len; // compute unit length normal
+      rec.set_face_normal(r, outward_normal);*/
+
+      return true;
    }
 
 public:
